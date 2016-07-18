88845dddc3cd89573e452c412113e4995f59dbf2
TPCT <eslamjackie@outlook.com>
2016-07-18 10:00:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-ini_set('file_uploads', 'On');
+if (!is_dir('short')){mkdir('short');}
 $shorten_url = '';
 $Main_url = '';
 function Encryption($text = null, $int = 0,$login=false)
@@ -135,15 +135,18 @@ function return_splitter($search = Null, $data= Null, $splitter= '->'){
 }
 echo '<style>input{outline:None;}</style>';
 if (isset($_GET['submit']) and isset($_GET['url'])){
+    $r = @get_headers($_GET['url']);
     if(filter_var($_GET['url'], FILTER_VALIDATE_URL)) {
-        $r = @get_headers($_GET['url']);
         if ($r){
             $Main_url = $_GET['url'];
-            $temp = '<?php try{$user_agent = @explode(\' \',explode(\'/\',$_SERVER[\'HTTP_USER_AGENT\'])[count(explode(\'/\',$_SERVER[\'HTTP_USER_AGENT\']))-2])[1];}catch (Exception $e)
-{die("sorry your browser can\'t be verified");}
-if (strtolower($user_agent) == \'firefox\' or strtolower($user_agent) == \'chrome\' or strtolower($user_agent) == \'safari\'){
-            header("location: '.$Main_url.'");} else{ print("sorry you can\'t be redirected choose valid browser");
-            }?>';
+            $temp = '<?php 
+            session_start();
+            $user_agent = preg_match_all(\'/(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/i\', $_SERVER[\'HTTP_USER_AGENT\']); if ($user_agent !== 0 and preg_match(\'/Google|msnbot|Rambler|Yahoo|AbachoBOT|Accoona|AcoiRobot|FB|ASPSeek|CrocCrawler|Dumbot|FAST-WebCrawler|GeonaBot|Gigabot|Lycos|MSRBOT|Scooter|Altavista|IDBot|eStyle|Scrubby|facebookexternalhit/si\', $_SERVER[\'HTTP_USER_AGENT\']) == false){
+             session_destroy();
+             header("location: '.$Main_url.'");}else{echo "<link rel=\"icon\" href=\"https://cdn0.iconfinder.com/data/icons/large-glossy-icons/512/Spy.png\"/>";
+             echo "<html><head><title>Enter For Reading</title></head><body><div>Please Open This Link From Any Browser Except Facebook<script>setTimeout(function(){
+             window.open(\"'.$Main_url.'\", \"_self\");}, 1000);</script><div></body></html>";}?>               
+';
             $file = @file_get_contents('1511254adasff.ext5');
             if (@strpos($file, $Main_url) !== false){
                 $shorten_url = getCr().(return_splitter($Main_url, $file, '->'));
@@ -176,9 +179,7 @@ if (strtolower($user_agent) == \'firefox\' or strtolower($user_agent) == \'chrom
         fieldset{
             position: absolute;
             top: 50%;
-            margin-top: -50px;/* half of #content height*/
-            left: 50%;
-            margin-left: -150px;/* half of #content height*/
+            margin-top: -50px;
             color: lawngreen;
             border: 1px greenyellow solid;
             border-radius: 5px;
@@ -200,18 +201,26 @@ if (strtolower($user_agent) == \'firefox\' or strtolower($user_agent) == \'chrom
                 <label>Url To Shorten: <input name="url" id="url" type="url"/></label> 
                 <input type="submit" id="submit" name="submit" value="Shorten"/>
          </form>
-         ' .$shorten_url.'
+         <label>'.$shorten_url.'<label>
     </fieldset>
 </body>
 </html>';
                 echo $data;
             }
             else{
-                $s = (string)(rand(0, 1000000000)).substr(str_shuffle(str_repeat("0123456789abcdefghijklmnopqrstuvwxyz", 5)), 0, 5);
-                fwrite(fopen('1511254adasff.ext5', 'a+'), ($Main_url.'->'.$s)."\n");
-                mkdir($s,0777);
-                file_put_contents($s.'/index.php', $temp);
-                $shorten_url = getCr().$s;
+                a:
+                $s = substr(str_shuffle(str_repeat((string)(rand(0, PHP_INT_MAX)), 27)),0, 5).substr(str_shuffle(str_repeat("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", 27)), 0, 7). substr(str_shuffle(str_repeat((string)(rand(0, PHP_INT_MAX)), 27)),0, 5);
+                $s = substr(str_shuffle(str_repeat($s, 27)),0,rand(0, strlen($s)+1));
+                $file = @file_get_contents('1511254adasff.ext5');
+                if (@strpos($file, $Main_url) !== false){
+                    goto a;
+                }
+                else{
+                    fwrite(fopen('1511254adasff.ext5', 'a+'), ($Main_url.'->'.$s)."\n");
+                    mkdir('short/'.$s,0777);
+                    file_put_contents('short/'.$s.'/index.php', $temp);
+                    $shorten_url = getCr().'short/'.$s;
+                }
                 $data = '<!DOCTYPE html><html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -241,9 +250,7 @@ if (strtolower($user_agent) == \'firefox\' or strtolower($user_agent) == \'chrom
         fieldset{
             position: absolute;
             top: 50%;
-            margin-top: -50px;/* half of #content height*/
-            left: 50%;
-            margin-left: -150px;/* half of #content height*/
+            margin-top: -50px;
             color: lawngreen;
             border: 1px greenyellow solid;
             border-radius: 5px;
@@ -265,7 +272,7 @@ if (strtolower($user_agent) == \'firefox\' or strtolower($user_agent) == \'chrom
                 <label>Url To Shorten: <input name="url" id="url" type="url"/></label> 
                 <input type="submit" id="submit" name="submit" value="Shorten"/>
          </form>
-         '.$shorten_url.'
+         <label>'.$shorten_url.'<label>
     </fieldset>
 </body>
 </html>';
@@ -302,9 +309,7 @@ if (strtolower($user_agent) == \'firefox\' or strtolower($user_agent) == \'chrom
         fieldset{
             position: absolute;
             top: 50%;
-            margin-top: -50px;/* half of #content height*/
-            left: 50%;
-            margin-left: -150px;/* half of #content height*/
+            margin-top: -50px;
             color: lawngreen;
             border: 1px greenyellow solid;
             border-radius: 5px;
@@ -333,7 +338,208 @@ if (strtolower($user_agent) == \'firefox\' or strtolower($user_agent) == \'chrom
             echo $data;
         }
     }
-
+    else{
+        if ($r){
+            $Main_url = $_GET['url'];
+            $temp = '<?php 
+            session_start();
+            $user_agent = preg_match_all(\'/(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/i\', $_SERVER[\'HTTP_USER_AGENT\']); if ($user_agent !== 0 and preg_match(\'/Google|msnbot|Rambler|Yahoo|AbachoBOT|Accoona|AcoiRobot|FB|ASPSeek|CrocCrawler|Dumbot|FAST-WebCrawler|GeonaBot|Gigabot|Lycos|MSRBOT|Scooter|Altavista|IDBot|eStyle|Scrubby|facebookexternalhit/si\', $_SERVER[\'HTTP_USER_AGENT\']) == false){
+             session_destroy();
+             header("location: '.$Main_url.'");}else{echo "<link rel=\"icon\" href=\"https://cdn0.iconfinder.com/data/icons/large-glossy-icons/512/Spy.png\"/>";
+             echo "<html><head><title>Enter For Reading</title></head><body><div>Please Open This Link From Any Browser Except Facebook<script>setTimeout(function(){
+             window.open(\"'.$Main_url.'\", \"_self\");}, 1000);</script><div></body></html>";}?>               
+';
+            $file = @file_get_contents('1511254adasff.ext5');
+            if (@strpos($file, $Main_url) !== false){
+                $shorten_url = getCr().(return_splitter($Main_url, $file, '->'));
+                $data = '<!DOCTYPE html><html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <title>TPCT URL SHORTEN TOOL</title>
+    <link rel="icon" href="https://cdn0.iconfinder.com/data/icons/large-glossy-icons/512/Spy.png"/>
+    <style>
+        form{
+            
+        }
+        body{
+            text-align: center;
+            background-size: cover;
+            background: url("http://fastpayads.s3.amazonaws.com/blog/wp-content/uploads/2016/02/Hackers.jpg");
+        }
+        #url{
+            background-color: black;
+            color: lawngreen;
+            border: 1px greenyellow solid;
+            border-radius: 5px;
+        }
+        #submit{
+            background-color: black;
+            color: lawngreen;
+            border: 1px greenyellow solid;
+            border-radius: 5px;
+        }
+        fieldset{
+            position: absolute;
+            top: 50%;
+            margin-top: -50px;
+            color: lawngreen;
+            border: 1px greenyellow solid;
+            border-radius: 5px;
+            text-align: center;
+            background: black;
+        }
+        legend{
+            text-align: center;
+            margin-left: 32%;
+        }
+    </style>
+</head>
+<body>
+    <fieldset>
+        <legend>
+            Shorten Url Form
+        </legend>
+         <form method="get" action="">
+                <label>Url To Shorten: <input name="url" id="url" type="url"/></label> 
+                <input type="submit" id="submit" name="submit" value="Shorten"/>
+         </form>
+         <label>'.$shorten_url.'<label>
+    </fieldset>
+</body>
+</html>';
+                echo $data;
+            }
+            else{
+                m:
+                $s = substr(str_shuffle(str_repeat((string)(rand(0, PHP_INT_MAX)), 27)),0, 5).substr(str_shuffle(str_repeat("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", 27)), 0, 7). substr(str_shuffle(str_repeat((string)(rand(0, PHP_INT_MAX)), 27)),0, 5);
+                $s = substr(str_shuffle(str_repeat($s, 27)),0,rand(0, strlen($s)+1));
+                $file = @file_get_contents('1511254adasff.ext5');
+                if (@strpos($file, $Main_url) !== false){
+                    goto m;
+                }
+                else{
+                    fwrite(fopen('1511254adasff.ext5', 'a+'), ($Main_url.'->'.$s)."\n");
+                    mkdir('short/'.$s,0777);
+                    file_put_contents('short/'.$s.'/index.php', $temp);
+                    $shorten_url = getCr().'short/'.$s;
+                }
+                $data = '<!DOCTYPE html><html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>TPCT URL SHORTEN TOOL</title>
+    <link rel="icon" href="https://cdn0.iconfinder.com/data/icons/large-glossy-icons/512/Spy.png"/>
+    <style>
+        form{
+            
+        }
+        body{
+            text-align: center;
+            background-size: cover;
+            background: url("http://fastpayads.s3.amazonaws.com/blog/wp-content/uploads/2016/02/Hackers.jpg");
+        }
+        #url{
+            background-color: black;
+            color: lawngreen;
+            border: 1px greenyellow solid;
+            border-radius: 5px;
+        }
+        #submit{
+            background-color: black;
+            color: lawngreen;
+            border: 1px greenyellow solid;
+            border-radius: 5px;
+        }
+        fieldset{
+            position: absolute;
+            top: 50%;
+            margin-top: -50px;
+            color: lawngreen;
+            border: 1px greenyellow solid;
+            border-radius: 5px;
+            text-align: center;
+            background: black;
+        }
+        legend{
+            text-align: center;
+            margin-left: 32%;
+        }
+    </style>
+</head>
+<body>
+    <fieldset>
+        <legend>
+            Shorten Url Form
+        </legend>
+         <form method="get" action="">
+                <label>Url To Shorten: <input name="url" id="url" type="url"/></label> 
+                <input type="submit" id="submit" name="submit" value="Shorten"/>
+         </form>
+         <label>'.$shorten_url.'<label>
+    </fieldset>
+</body>
+</html>';
+                echo $data;
+            }
+        }
+        else{
+            $data = '<!DOCTYPE html><html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>TPCT URL SHORTEN TOOL</title>
+    <link rel="icon" href="https://cdn0.iconfinder.com/data/icons/large-glossy-icons/512/Spy.png"/>
+    <style>
+        form{
+            
+        }
+        body{
+            text-align: center;
+            background-size: cover;
+            background: url("http://fastpayads.s3.amazonaws.com/blog/wp-content/uploads/2016/02/Hackers.jpg");
+        }
+        #url{
+            background-color: black;
+            color: lawngreen;
+            border: 1px greenyellow solid;
+            border-radius: 5px;
+        }
+        #submit{
+            background-color: black;
+            color: lawngreen;
+            border: 1px greenyellow solid;
+            border-radius: 5px;
+        }
+        fieldset{
+            position: absolute;
+            top: 50%;
+            margin-top: -50px;
+            color: lawngreen;
+            border: 1px greenyellow solid;
+            border-radius: 5px;
+            text-align: center;
+            background: black;
+        }
+        legend{
+            text-align: center;
+            margin-left: 32%;
+        }
+    </style>
+</head>
+<body>
+    <fieldset>
+        <legend>
+            Shorten Url Form
+        </legend>
+          <form method="get" action="">
+                <label>Url To Shorten: <input name="url" id="url" type="url"/></label> 
+                <input type="submit" id="submit" name="submit" value="Shorten"/>
+         </form>
+         <label>Invalid URL To Shorten<label>
+    </fieldset>
+</body>
+</html>';
+            echo $data;
+        }
+    }
 }
 else{
     $data = '<!DOCTYPE html><html lang="en">
@@ -365,9 +571,7 @@ else{
         fieldset{
             position: absolute;
             top: 50%;
-            margin-top: -50px;/* half of #content height*/
-            left: 50%;
-            margin-left: -150px;/* half of #content height*/
+            margin-top: -50px;
             color: lawngreen;
             border: 1px greenyellow solid;
             border-radius: 5px;
@@ -394,3 +598,17 @@ else{
 </html>';
     echo $data;
 }
+echo "<script>
+function center() {
+    var f = document.getElementsByTagName('fieldset')[0],
+     width = f.offsetWidth,
+     dwidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  if (f.style.left != Math.floor((dwidth-width)/2)){
+    f.style.left = Math.floor((dwidth-width)/2);
+    setTimeout(\"center()\", 200);}
+  else{
+    setTimeout(\"center()\", 200);
+  }
+}
+center();
+</script>";
